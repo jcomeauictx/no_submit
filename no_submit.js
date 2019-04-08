778e586bec18f5738855615e7d31512a024f8fd5
John Comeau <jc@unternet.net>
2019-04-07 22:19:36 -0700

    fixed some major bugs

--- no_submit.js
+++ no_submit.js
@@ -2,17 +2,21 @@ const submitText = {
 	"Submit": "Make it so!",
 	"submit": "make it so!"
 };
+const buttonTypes = {
+	"submit": "submit",
+	"button": "button"
+};
 // inefficient, but I can't figure out iterating over xpath results
 let result = document.querySelectorAll("button, input")
 for (let i = 0; i < result.length; i++) {
 	let node = result[i];
-	if (node.tagName == "INPUT") {
+	if (node.tagName == "INPUT" && node.type in buttonTypes) {
 		if (node.value in submitText) {
 			node.value = submitText[node.value];
 		} else if (!node.value) {
 			node.value = submitText["Submit"];
 		}
-	} else {
+	} else {  // must be BUTTON
 		if (node.innerText in submitText) {
 			node.innerText = submitText[node.innerText];
 		}
